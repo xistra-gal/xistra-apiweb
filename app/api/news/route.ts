34c2f8d226cd92fce821b173e9e28ea5fef794e5
Javier Martin <jmartinpizarro04@gmail.com>
2025-08-09 19:41:16 +0200

    hotfix

--- app/api/news/route.ts
+++ app/api/news/route.ts
@@ -9,36 +9,32 @@ export async function GET(request: Request) {
       .select('*')
       .limit(4)
       .order('created_at', { ascending: false })
-
     if (error) {
-      console.error('Error when fetching news:', error.message)
       return new Response(
         JSON.stringify({ error: 'Error when fetching news', details: error.message }),
         {
           status: 500,
-          headers: {'Content-Type': 'application/json', ...corsHeaders},
+          headers: { 'Content-Type': 'application/json', ...corsHeaders },
         }
       )
     }
-
     return new Response(JSON.stringify(data), {
       status: 200,
-      headers: {'Content-Type': 'application/json', ...corsHeaders},
+      headers: { 'Content-Type': 'application/json', ...corsHeaders },
     })
   } catch (err) {
-    console.error('Unidentified exception:', err)
     return new Response(
       JSON.stringify({ error: 'Server is fucked up' }),
       {
         status: 500,
-        headers: {'Content-Type': 'application/json', ...corsHeaders},
+        headers: { 'Content-Type': 'application/json', ...corsHeaders },
       }
     )
   }
 }
 
 // preflight CORS request (OPTIONS pa los amigos)
-export async function OPTIONS() {
+export async function OPTIONS(request: Request) {
   return new Response(null, {
     status: 204,
     headers: corsHeaders,
