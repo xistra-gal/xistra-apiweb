6c71baa6804bb25d64069798c2395bc0eea43f56
Javier Martín <jmartinpizarro04@gmail.com>
2025-09-04 09:48:50 +0200

    feat: remove email func

--- app/utils/resend/resend.ts
+++ app/utils/resend/resend.ts
@@ -13,23 +13,22 @@ export default async function addUserToNewsLetter(email: string) {
             .from("newsletter_members")
             .insert({ email: email })
 
-
         if (error) {
             throw new Error("Error when inserting the email in the db");
         }
         // send an email to notice the user
-        const response = await resend.emails.send({
-            from: 'onboarding@resend.dev', // change this when i have the domain account
-            to: email,
-            subject: 'Xistra Newsletter - ¡Bienvenido!',
-            html: `
-            <h2>¡Gracias por suscribirte!</h2>
-            <p>Ahora estarás al tanto de todas nuestras novedades sobre <strong>Xistra</strong>, sus noticias, investigaciones y acuerdos. ¡Serás el primero en enterarte!</p>
-            <p>Muchas gracias por suscribirte,</p>
-            <p>El equipo de <strong>Xistra</strong></>`
-        });
+        //const response = await resend.emails.send({
+        //    from: 'onboarding@resend.dev', // change this when i have the domain account
+        //    to: email,
+        //    subject: 'Xistra Newsletter - ¡Bienvenido!',
+        //    html: `
+        //    <h2>¡Gracias por suscribirte!</h2>
+        //    <p>Ahora estarás al tanto de todas nuestras novedades sobre <strong>Xistra</strong>, sus noticias, investigaciones y acuerdos. ¡Serás el primero en enterarte!</p>
+        //    <p>Muchas gracias por suscribirte,</p>
+        //    <p>El equipo de <strong>Xistra</strong></>`
+        //});
 
-        return response;
+        return data;
     } catch (error) {
         throw new Error("Unexpected error when sending the email!");
     }
